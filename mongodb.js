5de2e5e568e99995a5f426ff9a75684dc7199851
Benson <tumlivein@gmail.com>
2019-10-06 23:38:29 +0800

    80. Updating Documents: updateOne() Method

--- mongodb.js
+++ mongodb.js
@@ -12,17 +12,15 @@ MongoClient.connect(url, { useNewUrlParser: true }, (error, client) => {
 
     const db = client.db(dbName);
 
-    // 
-    // Goal: Use find and findOne with tasks
-    // 
-    // 1. Use findOne to fetch the last task by its id (print doc to console)
-    // 2. Use find to fetch all tasks that are not completed (print docs to console)
-    // 3. Test your work!
-    db.collection('tasks').findOne({ _id: new ObjectID('5d99eba64da18c0d3229a32a')}, (error, result) => {
-        console.log(result);
-    });
-
-    db.collection('tasks').find({ completed: false }).toArray((error, docs) => {
-        console.log(docs);
-    });
+    db.collection('users')
+        .updateOne(
+            { _id: new ObjectID('5d9970c6b23cde05c2988073')},
+            { $set: {
+                name: 'Joy',
+            } },
+        ).then((result) => {
+            console.log(result);
+        }).catch((error) => {
+            console.log(error);
+        });
 });
