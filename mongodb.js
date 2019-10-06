a1fa93e59ec77149d9963282f0d421609e9a93e2
Benson <tumlivein@gmail.com>
2019-10-06 13:46:27 +0800

    Challenge

--- mongodb.js
+++ mongodb.js
@@ -16,20 +16,35 @@ MongoClient.connect(url, { useNewUrlParser: true }, (error, client) => {
     // MongoDB will automatically generate a database for us
     const db = client.db(dbName);
 
-    db.collection('users').insertMany([
-        {
-            name: 'Benson',
-            age: 25,
-        },
-        {
-            name: 'Rebecca',
-            age: 29,
-        }
-    ], (error, result) => {
-        if (error) {
-            return console.log('Unable to insert to users collection');
-        }
+    // 
+    // Goal: Insert 3 tasks into a new task collection
+    // 
+    // 1. Use insertMany  to insert three documents
+    //    - description (string), completed (boolean)
+    // 2. Setup the callback to handle error or print ops
+    // 3. Run the script
+    // 4. Refresh the database in Robo 3t and view data in tasks collection
 
-        console.log(result.ops);
-    });
+    db
+        .collection('tasks')
+        .insertMany([
+           {
+                description: 'Task 1',
+                completed: false,
+           },
+           {
+                description: 'Task 2',
+                completed: true,
+           },
+           {
+                description: 'Task 3',
+                completed: false,
+           }
+        ], (error, result) => {
+            if (error) {
+                return console.log('Uable to insert into Task collection');
+            }
+
+            console.log(result.ops);
+        });
 });
