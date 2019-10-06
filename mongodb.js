5f1fe43d16e174b566f97e5cfe251b082a2400aa
Benson <tumlivein@gmail.com>
2019-10-06 20:58:39 +0800

    77. The ObjectID: ID to Hex

--- mongodb.js
+++ mongodb.js
@@ -4,24 +4,27 @@ const moment  = require('moment');
 const url = 'mongodb://127.0.0.1/27017';
 const dbName = 'task-manager';
 const id = new ObjectID();
+console.log(id, id.id.length);
+console.log(id.toHexString(), id.toHexString().length);
+console.log(id);
 
 MongoClient.connect(url, { useNewUrlParser: true }, (error, client) => {
     if (error) {
         return error;
     }
 
-    console.log(id);
-    console.log(id.generationTime); // return timestamp in seconds
-    console.log(moment(id.generationTime * 1000).format('YYYY MMMM Do, h:mm:ss a')); // use timestamp in millsecond * 1000 to transfer into seconds
-    console.log(moment.unix(id.generationTime).format('YYYY MMMM Do, h:mm:ss a')); // use moment.unix() to transfer into seconds
-    console.log(id.getTimestamp());
+//     console.log(id);
+//     console.log(id.generationTime); // return timestamp in seconds
+//     console.log(moment(id.generationTime * 1000).format('YYYY MMMM Do, h:mm:ss a')); // use timestamp in millsecond * 1000 to transfer into seconds
+//     console.log(moment.unix(id.generationTime).format('YYYY MMMM Do, h:mm:ss a')); // use moment.unix() to transfer into seconds
+//     console.log(id.getTimestamp());
 
-   const db = client.db(dbName);
+//    const db = client.db(dbName);
    
-   db.collection('users').insertOne({
-       name: 'Betty',
-       age: 28,
-   }, (error, result) => {
-       console.log(result.ops);
-   });
+//    db.collection('users').insertOne({
+//        name: 'Betty',
+//        age: 28,
+//    }, (error, result) => {
+//        console.log(result.ops);
+//    });
 });
