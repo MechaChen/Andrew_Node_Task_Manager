356671dad8d8adec0fba7b28e826176000bf032a
Benson <tumlivein@gmail.com>
2019-10-07 00:05:14 +0800

    81. Deleting Documents

--- mongodb.js
+++ mongodb.js
@@ -12,34 +12,12 @@ MongoClient.connect(url, { useNewUrlParser: true }, (error, client) => {
 
     const db = client.db(dbName);
 
-    // db.collection('users')
-    //     .updateOne(
-    //         { _id: new ObjectID('5d9970c6b23cde05c2988073')},
-    //         { $inc: {
-    //             age: 1,
-    //         } },
-    //     ).then((result) => {
-    //         console.log(result);
-    //     }).catch((error) => {
-    //         console.log(error);
-    //     });
-
-    // 
-    // Goal: Use updateMany to complete all tasks
-    // 
-    // 1. Check the documentation for updateMany
-    // 2. Setup the call with the query and the updates
-    // 3. Use promise methods to setup the success/error handlers
-    // 4. Test your work!
-
-    db.collection('tasks')
-        .updateMany(
-            { completed: false }, 
-            { $set: { completed: true } })
-        .then((result) => {
+    db.collection('users')
+        .deleteMany({
+            name: 'My First name'
+        }).then((result) => {
             console.log(result);
-        })
-        .catch((error) => {
+        }).catch((error) => {
             console.log(error);
         });
 });
