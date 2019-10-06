ffd99b2378546f4b5c784a83db3a6bea3484c4ee
Benson <tumlivein@gmail.com>
2019-10-06 22:32:10 +0800

    Challenge

--- mongodb.js
+++ mongodb.js
@@ -4,9 +4,6 @@ const moment  = require('moment');
 const url = 'mongodb://127.0.0.1/27017';
 const dbName = 'task-manager';
 const id = new ObjectID();
-console.log(id, id.id.length);
-console.log(id.toHexString(), id.toHexString().length);
-console.log(id);
 
 MongoClient.connect(url, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -15,13 +12,17 @@ MongoClient.connect(url, { useNewUrlParser: true }, (error, client) => {
 
     const db = client.db(dbName);
 
-    // collection.find() return a 'Cursor'
-
-    db.collection('users').find({ age: 25 }).toArray((error, docs) => {
-        console.log(docs);
+    // 
+    // Goal: Use find and findOne with tasks
+    // 
+    // 1. Use findOne to fetch the last task by its id (print doc to console)
+    // 2. Use find to fetch all tasks that are not completed (print docs to console)
+    // 3. Test your work!
+    db.collection('tasks').findOne({ _id: new ObjectID('5d99eba64da18c0d3229a32a')}, (error, result) => {
+        console.log(result);
     });
 
-    db.collection('users').find({ age: 25 }).count((error, count) => {
-        console.log(count);
+    db.collection('tasks').find({ completed: false }).toArray((error, docs) => {
+        console.log(docs);
     });
 });
