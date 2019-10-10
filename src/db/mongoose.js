54bb827a781e8b6df3b084760b2acf8535dfbac1
Benson <tumlivein@gmail.com>
2019-10-10 13:14:45 +0800

    85. Data Validation and Sanitization: Part l: Custom Validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -9,14 +9,21 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
     name: {
         type: String,
-        required: true,
+        required: [true, 'There is no user without name'],
     },
     age: {
         type: Number,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number');
+            }
+        }
     },
 });
 
 const user = new User({
+    name: 'Benson1',
+    age: -1,
 });
 
 user.save()
