d90ed49bb603258d0e26006768dcc493efa040b5
Benson Chen <tumlivein@gmail.com>
2019-10-10 19:53:36 +0800

    86. Data Validation and Sanitization: Part ll: Challenge: Task Validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,60 +7,79 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true,
 });
 
-// 
-// Goal: Add a password field to User
-// 
-// 1. Setup the field as a required string
-// 2. Ensure the length is greater than 6
-// 3. Trim the password
-// 4. Ensure that password doesn't contain "password"
-// 5. Test your work!
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String,
+//         trim: true,
+//         required: [true, 'There is no user without name'],
+//     },
+//     email: {
+//         type: String,
+//         require: true,
+//         trim: true,
+//         lowercase: true,
+//         validate(value) {
+//             if (!validator.isEmail(value)) {
+//                 throw new Error('Email is not valid');
+//             }
+//         }
+//     },
+//     password: {
+//         type: String,
+//         require: true,
+//         trim: true,
+//         minlength: 7,
+//         validate(value) {
+//             if (value.toLowerCase().includes('password')) {
+//                 throw new Error('Your password can not contain the word "password"');
+//             }
+//         }
+//     },
+//     age: {
+//         type: Number,
+//         default: 0,
+//         validate(value) {
+//             if (value < 0) {
+//                 throw new Error('Age must be a positive number');
+//             }
+//         }
+//     },
+// });
 
-const User = mongoose.model('User', {
-    name: {
+// const user = new User({
+//     name: '   BENSON   ',
+//     email: 'TUMLIVEIN@GMAIL.COM     ',
+//     password: '78cxepaordwrixc',
+// });
+// 
+// user.save()
+//     .then((user) => console.log(user))
+//     .catch((err) => console.log(chalk.bgRed(err.message)));
+
+
+// 
+// Goal: Add validation and sanization to task
+// 
+// 1. Trim the description and make it required
+// 2. Make completed optional and default it to false
+// 3. Test your work with and without errors
+
+const Task = mongoose.model('Task', {
+    description: {
         type: String,
+        required: true,
         trim: true,
-        required: [true, 'There is no user without name'],
     },
-    email: {
-        type: String,
-        require: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is not valid');
-            }
-        }
-    },
-    password: {
-        type: String,
-        require: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Your password can not contain the word "password"');
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number');
-            }
-        }
+    completed: {
+        type: Boolean,
+        default: false,
     },
 });
 
-const user = new User({
-    name: '   BENSON   ',
-    email: 'TUMLIVEIN@GMAIL.COM     ',
-    password: '78cxepaordwrixc',
+const task = new Task({
+    description: 'New Task',
 });
 
-user.save()
-    .then((user) => console.log(user))
-    .catch((err) => console.log(chalk.bgRed(err.message)));
+task.save()
+    .then((doc) => console.log(doc))
+    .catch((err) => console.log(err));
