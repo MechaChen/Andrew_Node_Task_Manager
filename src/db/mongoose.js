9f55eecc6b4fb6dfe2224b894f6a0162e60d873c
Benson <tumlivein@gmail.com>
2019-10-10 13:35:26 +0800

    85. Data Validation and Sanitization: Part l: Build in SchemaTypes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -10,11 +10,14 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
     name: {
         type: String,
+        trim: true,
         required: [true, 'There is no user without name'],
     },
     email: {
         type: String,
         require: true,
+        trim: true,
+        lowercase: true,
         validate(value) {
             if (!validator.isEmail(value)) {
                 throw new Error('Email is not valid');
@@ -23,6 +26,7 @@ const User = mongoose.model('User', {
     },
     age: {
         type: Number,
+        default: 0,
         validate(value) {
             if (value < 0) {
                 throw new Error('Age must be a positive number');
@@ -32,8 +36,8 @@ const User = mongoose.model('User', {
 });
 
 const user = new User({
-    name: 'Benson1',
-    email: 'tumlivein@',
+    name: '   BENSON   ',
+    email: 'TUMLIVEIN@GMAIL.COM     ',
 });
 
 user.save()
