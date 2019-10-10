68cc1b45bcc59ca8c182f926c288e72ca08a9945
Benson <tumlivein@gmail.com>
2019-10-10 13:03:41 +0800

    85. Data Validation and Sanitization: Part l: Required Validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,28 +1,24 @@
 const mongoose = require('mongoose');
+const chalk = require('chalk');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', { 
     useNewUrlParser: true,
     useCreateIndex: true,
 });
 
-// 
-// Goal: Create a model for tasks
-// 
-// 1. Define the model with description and completed fields
-// 2. Create a new instance of the model
-// 3. Save the model to the database
-// 4. Test your work!
-
-const Task = new mongoose.model('Task', {
-    description: { type: String },
-    completed: { type: Boolean },
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+    },
 });
 
-const task = new Task({
-    description: 'task',
-    completed: true,
+const user = new User({
 });
 
-task.save()
-    .then((task) => console.log(task))
-    .catch((err) => console.log(err));
+user.save()
+    .then((user) => console.log(user))
+    .catch((err) => console.log(chalk.bgRed(err.message)));
