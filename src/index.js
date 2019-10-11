1f07256c161fee74bfb8c1680c29084a741c26c1
Benson <tumlivein@gmail.com>
2019-10-11 18:21:43 +0800

    92. Resource Reading Endpoints: Part ll

--- src/index.js
+++ src/index.js
@@ -55,4 +55,39 @@ app.post('/tasks', async (req, res) => {
     }
 });
 
+// 
+// Goal: Setup the task reading endpoints
+// 
+// 1. Create an endpoint for fetching all tasks
+// 2. Create an endpoint for fetching a task by its id
+// 3. Setup new requests in Postman and test your work
+
+app.get('/tasks', (req, res) => {
+    Task.find({})
+        .then((docs) => {
+            if (!docs) {
+                return res.status(404).send(docs);
+            }
+            res.send(docs);
+        })
+        .catch((ex) => {
+            res.status(500).send(ex);
+        });
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+
+    Task.findById(_id)
+        .then((doc) => {
+            if (!doc) {
+                return res.status(404).send();
+            }
+            res.send(doc);
+        })
+        .catch((err) => {
+            res.status(500).send(err);
+        });
+});
+
 app.listen(port, () => console.log(`Server is up on port ${port}`));
