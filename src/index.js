72b667e1228c4f6f043d76ed80d28808c9caa54f
Benson <tumlivein@gmail.com>
2019-10-12 15:16:41 +0800

    Challenge

--- src/index.js
+++ src/index.js
@@ -45,43 +45,52 @@ app.get('/users/:id', async (req, res) => {
     }
 });
 
+// 
+// Goal: Refactor task routes to use async/await
+// 
+// 1. Refactor task routes to await/async
+// 2. Test all routes in Postman
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
 
     try {
-        const newTask = await task.save();
-        res.status(201).send(newTask);
+        const savedTask = await task.save();
+        res.status(201).send(savedTask);
     } catch (ex) {
         res.status(400).send(ex);
     }
 });
 
-app.get('/tasks', (req, res) => {
-    Task.find({})
-        .then((docs) => {
-            if (!docs) {
-                return res.status(404).send(docs);
-            }
-            res.send(docs);
-        })
-        .catch((ex) => {
-            res.status(500).send(ex);
-        });
+app.get('/tasks', async (req, res) => {
+
+    try {
+        const findedTasks = await Task.find({});
+
+        if (!findedTasks) {
+            return res.status(404).send();
+        }
+
+        res.send(findedTasks);
+    } catch (e) {
+        res.status(500).send(e);
+    }
 });
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id;
 
-    Task.findById(_id)
-        .then((doc) => {
-            if (!doc) {
-                return res.status(404).send();
-            }
-            res.send(doc);
-        })
-        .catch((err) => {
-            res.status(500).send(err);
-        });
+    try {
+        const findedTask = await Task.findById(_id);
+
+        if (!findedTask) {
+            return res.status(404).send();
+        }
+
+        res.send(findedTask);
+    } catch (e) {
+        res.status(500).send(e);
+    }
 });
 
 app.listen(port, () => console.log(`Server is up on port ${port}`));
