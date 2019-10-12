29ba324928f263f78b77ace5e031c6c4d32e4521
Benson <tumlivein@gmail.com>
2019-10-12 20:51:10 +0800

    Challenge

--- src/index.js
+++ src/index.js
@@ -110,4 +110,38 @@ app.get('/tasks/:id', async (req, res) => {
     }
 });
 
+
+// 
+// Goal: Allow for task updates
+// 
+// 1. Setup the route handler
+// 2. Send error if unknown updates
+// 3. Attempt to update the task
+//    - Handle task not found
+//    - Handle validation errors
+//    - Handle success
+// 4. Test your work!
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' });
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
 app.listen(port, () => console.log(`Server is up on port ${port}`));
