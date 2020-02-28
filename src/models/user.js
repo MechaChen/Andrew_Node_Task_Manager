6e3673a8c95b9ba7913553adc65bb6e91a47d559
Benson <tumlivein@gmail.com>
2020-02-28 21:11:57 +0800

    104. Securely Storing Passwords: Part II

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-const User = mongoose.model('User', {
+const userSchema = mongoose.Schema({
     name: {
         type: String,
         trim: true,
@@ -39,5 +40,19 @@ const User = mongoose.model('User', {
         }
     },
 });
+
+userSchema.pre('save', async function(next) {
+    const user = this;
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+
+    console.log('just before saving');
+
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
