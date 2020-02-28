1e95b8e7175c1ca92ef99148429ef581c5463e65
Benson <tumlivein@gmail.com>
2020-02-28 21:19:44 +0800

    Challenge

--- src/routers/task.js
+++ src/routers/task.js
@@ -45,6 +45,14 @@ router.get('/tasks/:id', async (req, res) => {
     }
 });
 
+// 
+// Goal: Change how tasks are updated
+// 
+// 1. Find the task
+// 2. Alter the task properties
+// 3. Save the task
+// 4. Test your work by updating a task from Postman
+
 router.patch('/tasks/:id', async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['description', 'completed'];
@@ -55,7 +63,11 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        const task = await Task.findById(req.params.id);
+        
+        updates.forEach((update) => task[update] = req.body[update]);
+        await task.save();
+        // const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
 
         if (!task) {
             return res.status(404).send();
