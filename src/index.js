6d089139165b9a2ac375e40074a2f4d5923b9be8
Benson <tumlivein@gmail.com>
2020-02-29 12:34:48 +0800

    108. Express Middleware

--- src/index.js
+++ src/index.js
@@ -6,9 +6,23 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    if (req.method === 'GET') {
+        res.send('GET request are disabled');
+    } else {
+        next();
+    }
+})
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+
+// 
+// Without middleware: new request -> run route handle
+// 
+// With middleware: new request -> do something -> run route handler
+// 
 
 const bcrypt = require('bcryptjs');
 
