cf270f4a2663ba9b6f995669d3fde332024d1a19
Benson Chen <tumlivein@gmail.com>
2020-03-11 17:52:24 +0800

    121. Sorting Data

--- src/routers/task.js
+++ src/routers/task.js
@@ -30,12 +30,19 @@ router.post('/tasks', auth, async (req, res) => {
 
 
 // GET /tasks?completed=true
-// GEt /tasks?limit=10&skip=0
+// GET /tasks?limit=10&skip=0
+// GET /tasks?sortBy=createdAt_desc
 router.get('/tasks', auth, async (req, res) => {
-    const match = {}
+    const match = {};
+    const sort = {};
 
     if (req.query.completed) {
         match.completed = req.query.completed === 'true';
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
     }
 
     try {
@@ -44,7 +51,8 @@ router.get('/tasks', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate();
 
