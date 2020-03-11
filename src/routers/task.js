870dca058da17e3e24384a7df3f575735ba0f29f
Benson Chen <tumlivein@gmail.com>
2020-03-11 15:05:17 +0800

    Challenge: Refactor Patch /tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -53,7 +53,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
     }
 });
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['description', 'completed'];
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
@@ -63,14 +63,14 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findById(req.params.id);
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id });
         
-        updates.forEach((update) => task[update] = req.body[update]);
-        await task.save();
-
         if (!task) {
             return res.status(404).send();
         }
+
+        updates.forEach((update) => task[update] = req.body[update]);
+        await task.save();
 
         res.send(task);
     } catch (e) {
