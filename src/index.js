7fece66ea4db24a90053f84b6f6721f2c11cee90
Benson Chen <tumlivein@gmail.com>
2020-03-12 17:39:13 +0800

    124. Validating File Uploads

--- src/index.js
+++ src/index.js
@@ -9,12 +9,21 @@ const port = process.env.PORT || 3000;
 
 const multer = require('multer');
 const upload = multer({ 
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.docx?$/)) {
+            return cb(new Error('Please upload a Word document'));
+        }
+
+        cb(undefined, true);
+    }
 });
 app.post('/upload', upload.single('upload'), (req, res) => {
     res.send();
 });
-
 
 
 app.use(express.json());
