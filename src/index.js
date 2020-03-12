9638e1a2da1c0af69f8f8f4b822545c418a7c5f0
Benson Chen <tumlivein@gmail.com>
2020-03-12 16:53:31 +0800

    123. Adding Support for File Uploads

--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+const multer = require('multer');
+const upload = multer({ 
+    dest: 'images'
+});
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send();
+});
+
+
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
