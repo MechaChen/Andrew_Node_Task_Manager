b2054a71f51460059fa3ed50cfdad313bda72dda
Benson Chen <tumlivein@gmail.com>
2020-03-13 15:11:22 +0800

    Challenge

--- src/routers/user.js
+++ src/routers/user.js
@@ -102,4 +102,18 @@ router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) 
     res.status(400).send({ error: error.message });
 });
 
+// 
+// Goal: Setup route to delete avatar
+// 
+// 1. Setup DELETE /users/me/avatar
+// 2. Add authentication
+// 3. Set the field to undefined and save the user sending back a 200
+// 4. Test your work by creating new request for Task App in Postman
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
+});
+
 module.exports = router;
